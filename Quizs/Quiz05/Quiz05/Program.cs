2773f5bba80f4a4a012ea6f004a65b5ae9795896
Steven Fairchild <steve51516@gmail.com>
2020-08-05 08:24:15 -0400

    Added Quiz05

---
+++ Quizs/Quiz05/Quiz05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quiz05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragram = "The quick brown fox jumps over the lazy dog";
+
+            string[] words = paragram.Split(' ');
+
+            Array.Reverse(words);
+
+            for (int i = 0; i < words.Length; i++)
+                Console.WriteLine($"{i} {words[i]}");
+        }
+    }
+}
