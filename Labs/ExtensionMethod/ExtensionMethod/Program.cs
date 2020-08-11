448c58325c45f0c58747058443bb56ac1c22dbfc
Steven Fairchild <steve51516@gmail.com>
2020-08-11 11:32:59 -0400

    added Extension lab

---
+++ Labs/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using Extensions;
+using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("This is lab extension methods");
+            Console.WriteLine("Enter an interger to convert from: ");
+            string input = Console.ReadLine();
+            int x = int.Parse(input);
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.WriteLine($"{x} in base {i} is {x.ConvertToBase(i)}");
+            }
+        }
+
+        static void Main()
+        {
+                doWork();
+        }
+    }
+}
